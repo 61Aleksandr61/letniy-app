6bf1ff4d999645cbf08ac7da4983293098319e6c
61Aleksandr61 <148265338+61Aleksandr61@users.noreply.github.com>
2023-11-02 20:25:38 +0300

    responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,11 @@
 import { NavLink, Link } from 'react-router-dom'
 import Logo from '../img/logo.svg'
+import {useState} from "react";
 
 const Header = () => {
+
+    const [isOpen, setIsOpen] = useState(false)
+
     return(
         <div className="header">
             <div className="container">
@@ -10,8 +14,8 @@ const Header = () => {
                         <Link to={"/"} className="header-logo">
                             <img src={Logo} alt=""/>
                         </Link>
-                    </div>                  
-                    <div className="navigation">
+                    </div>
+                    <div className={`navigation ${isOpen ? "open" : ""}`}>
                         <ul className="navigation__list">
                             <li className="navigation__item">
                                 <NavLink to={"/"} className="navigation__link link">Главная</NavLink>
@@ -34,7 +38,7 @@ const Header = () => {
                         </ul>
                     </div>
                     <div className='phones'>
-                        <div className='phone-namber'>
+                        <div className='phone-number'>
                             <p>Отдел продаж</p>
                             <Link to={"tel:+78126059146"} className="phone-link link">+7 (812) 605-91-46</Link>
                         </div>
@@ -42,6 +46,11 @@ const Header = () => {
                             <button className='callback-btn'>Заказать звонок</button>
                         </div>     
                     </div>
+                    <svg className="burger-menu" width="24px" height="24px" viewBox="0 0 24 24" fill="none" onClick={() => {setIsOpen(!isOpen)}}>
+                        <path d="M4 18L20 18" stroke="#000000" strokeWidth="2" strokeLinecap="round"/>
+                        <path d="M4 12L20 12" stroke="#000000" strokeWidth="2" strokeLinecap="round"/>
+                        <path d="M4 6L20 6" stroke="#000000" strokeWidth="2" strokeLinecap="round"/>
+                    </svg>
                 </div>
             </div>
         </div>
